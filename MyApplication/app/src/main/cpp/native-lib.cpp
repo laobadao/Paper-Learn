8a8e49e06f36f5ce6fb7b61c53becdc875ece41b
2296616900@qq.com <2296616900@qq.com>
2017-05-03 15:11:30 +0800

    init my application

---
+++ MyApplication/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,11 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_demo_zhaojun_practiceapplication_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
